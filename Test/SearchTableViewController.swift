514131f55ababcaacb832008664076121f09b064
mg <mikejgleason@gmail.com>
2017-04-19 19:36:51 -0400

    initial commit

--- Test/SearchTableViewController.swift
+++ Test/SearchTableViewController.swift
@@ -7,89 +7,56 @@
 //
 
 import UIKit
+import MapKit
 
-class SearchTableViewController: UITableViewController {
+protocol HandleMapSearch    {
+    func dropPinZoomIn(placemark: MKAnnotation)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+class SearchTableViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var mapView: MKMapView? = nil
+    var matchingItems = [MKAnnotation]()
+    var handleMapSearchDelegate:HandleMapSearch? = nil
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        matchingItems = []
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        
+        for item in self.mapView!.annotations    {
+            let title = item.title! as! String
+            
+            if title.hasPrefix(searchBarText) && searchBarText != ""  {
+                matchingItems.append(item)
+            }
+        }
+        self.tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return matchingItems.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        
+        let selectedItem = matchingItems[indexPath.row]
+        cell.textLabel?.text = selectedItem.title!
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row]
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var searchBar: UISearchBar!
+    private var label: UILabel!
+    
+    var searchController:UISearchController! = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        let location = CLLocation(latitude: 40.705260, longitude: -74.005515)
+        updateMap(location)
+        
+        let currentLoc = MKPointAnnotation()
+        currentLoc.title = "Maiden ln"
+        currentLoc.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let currentLocAnnotation = MKPinAnnotationView(annotation: currentLoc, reuseIdentifier: nil)
+        mapView.addAnnotation(currentLocAnnotation.annotation!)
+        
+        let Loews = MKPointAnnotation()
+        Loews.title = "Loews"
+        Loews.coordinate = CLLocationCoordinate2D(latitude: 40.774975, longitude: -73.981640)
+        let LoewsAnnotation = MKPinAnnotationView(annotation: Loews, reuseIdentifier: nil)
+        mapView.addAnnotation(LoewsAnnotation.annotation!)
+        
+        let Loews34 = MKPointAnnotation()
+        Loews34.title = "Loews34"
+        Loews34.coordinate = CLLocationCoordinate2D(latitude: 40.752452, longitude: -73.994869)
+        let Loews34Annotation = MKPinAnnotationView(annotation: Loews34, reuseIdentifier: nil)
+        mapView.addAnnotation(Loews34Annotation.annotation!)
+        
+        
+        let searchTable = storyboard!.instantiateViewController(withIdentifier: "SearchTableViewController") as! SearchTableViewController
+        
+        searchController = UISearchController(searchResultsController: searchTable)
+        searchController?.searchResultsUpdater = searchTable
+        searchController?.hidesNavigationBarDuringPresentation = false
+        searchController?.dimsBackgroundDuringPresentation = true
+        
+        definesPresentationContext = true
+        
+        let searchBar = searchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Input text"
+        navigationItem.titleView = searchController?.searchBar
+        
+        searchTable.mapView = mapView
+        searchTable.handleMapSearchDelegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateMap(_ location: CLLocation)  {
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 1000, 1000)
+        mapView.setRegion(region, animated: true)
     }
-
-
+    
+}
+extension ViewController: HandleMapSearch   {
+    func dropPinZoomIn(placemark: MKAnnotation)    {
+        updateMap(CLLocation(latitude: placemark.coordinate.latitude, longitude: placemark.coordinate.longitude))
+        searchController.searchBar.text = placemark.title!
+    }
 }
 
